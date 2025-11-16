7020d2b0842afa633f07fa0b2cbd951edbe04331
mohamed.haloka <mohamed.haloka@nahdetmisr.com>
2025-11-16 08:13:06 +0200

    Add google credential json path n envadd engine

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,13 +1,18 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
 
-const serviceAccount = JSON.parse(
-  readFileSync(new URL(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON, import.meta.url))
-);
+let serviceAccount;
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+try {
+  serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+} catch (e) {
+  console.error("❌ Failed to parse Firebase service account JSON:", e);
+}
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
 const db = admin.firestore();
 export default db;
